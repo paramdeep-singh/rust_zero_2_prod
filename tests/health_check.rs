76afd4de0bfea72ac1bd0271da8c8932fb03663d
Paramdeep Singh <paramdeep@meta.com>
2024-07-27 16:00:25 -0700

    added serde; impl tests for subscription post; skeleton for subscription post handler

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,6 +16,52 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn test_200_for_valid_subscription_post_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=param&email=param%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscription", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request!");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn test_400_when_subscription_request_lacks_required_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("email=param%40gmail.com", "name missing"),
+        ("name=param", "email missing"),
+        ("", "both name, email missing"),
+    ];
+
+    for (bad_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscription", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(bad_body)
+            .send()
+            .await
+            .expect("Failed to execute request!");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The subscription API did not return a 400 Bad Request response for the payload: {}",
+            error_message,
+        );
+    }
+}
+
 // launch app in the background
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
