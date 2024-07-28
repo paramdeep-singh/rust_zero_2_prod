d41e2abb66b74b4bbda5924ca65ef29aaa87230e
Paramdeep Singh <paramdeep@meta.com>
2024-07-28 11:53:49 -0700

    main now reads from a config at startup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
-use zero2prod::startup;
+use zero2prod::{configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = configuration::get_configuration()
+        .expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
     startup::run(listener)?.await
 }
