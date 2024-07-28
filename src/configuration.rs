b67eb327d00f2d8cc3056c088e04da60cc302016
Paramdeep Singh <paramdeep@meta.com>
2024-07-28 11:54:26 -0700

    fixed formatting

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,10 +14,11 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings =config::Config::builder()
-        .add_source(
-            config::File::new("configuration.yaml", config::FileFormat::Yaml)
-        )
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use zero2prod::{configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = configuration::get_configuration()
-        .expect("Failed to read configuration.");
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
