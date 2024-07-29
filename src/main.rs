ce1531df8128c41d9636271994e18c25848b7bb0
Paramdeep Singh <paramdeep@meta.com>
2024-07-29 16:50:16 -0700

    env specific configuration

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), std::io::Error> {
     )
     .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     startup::run(listener, connection_pool)?.await?;
     Ok(())
