a603ef201342c9ec130c176289c46a682ba338b8
Paramdeep Singh <paramdeep@meta.com>
2024-07-29 16:01:08 -0700

    lazily connect to db in main

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,9 @@ async fn main() -> Result<(), std::io::Error> {
     telemetry::init_subscriber(subscriber);
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
+    let connection_pool = PgPool::connect_lazy(
         configuration.database.connection_string().expose_secret(),
     )
-    .await
     .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
